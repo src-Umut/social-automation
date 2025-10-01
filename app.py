e7bc28e983c1a24cb9d72163996d351fdf0af899
src-Umut <umuterensurucu1903@gmail.com>
2025-10-01 03:53:04 +0300

    Add backend database setup and basic routes
    - Added get_db_connection() function for SQLite
    - Created init_db() to initialize posts table
    - Added / and /about routes
    - Prepared /new route structure (GET and POST)
    - Flask app skeleton ready for post listing and creation

--- app.py
+++ app.py
@@ -1,5 +1,27 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , request , redirect , url_for
+from config import DB_PATH
+import sqlite3
 app = Flask(__name__)
+
+
+def get_db_connection():
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.row
+    return conn 
+
+def init_db():
+    conn = get_db_connection()
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS posts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            content TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+init_db()
 
 @app.route("/")
 def home():
@@ -8,5 +30,23 @@ def home():
 def about():
     return "<h2>About Page</h2>"
 
+@app.route("/new" , methods = ["GET" , "POST"])
+def new_post():
+    if request.method == "POST":
+        title = request.form["title"]
+        content =  request.form["content"]
+        conn = get_db_connection
+        conn.execute("INSERT INTO posts (title, content) VALUES (?, ?)", (title, content))
+        conn.commit()
+        conn.close()
+        return redirect(url_for("home"))
+    return render_template("new_post.html")
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DB_PATH = os.path.join(BASE_DIR, "database", "app.db")
